d5bf5ae47d85eee12ba0c05d5845e78fcf930bd0
palagdan <daniilpalaginjob@gmail.com>
2025-09-02 15:34:12 +0200

    Implement button transition on hover

--- src/components/buttons/BorderMagicButton.tsx
+++ src/components/buttons/BorderMagicButton.tsx
@@ -1,15 +1,16 @@
-import React, { ReactNode, MouseEventHandler } from 'react';
+import React, {ReactNode, MouseEventHandler} from 'react';
 
 interface BorderMagicButtonProps {
     children: ReactNode;
     onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-const BorderMagicButton: React.FC<BorderMagicButtonProps> = ({ children, onClick }) => {
+const BorderMagicButton: React.FC<BorderMagicButtonProps> = ({children, onClick}) => {
     return (
         <button
             onClick={onClick}
-            className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+            className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
+            hover:translate-x-[-4px] hover:translate-y-[-4px] hover:shadow-[3px_3px_0px_white] transition-all duration-200"
         >
       <span
           className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"
